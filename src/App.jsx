ebe48a0f313af1851a94195fd1f3cd43ec4614d7
sheeba <sheebabalan2705@gmail.com>
2024-06-26 19:36:19 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import AdminDashboard from "./components/AdminDashboard";
+import "./App.css";// Import your Css file
+const App = () => {
+    return (
+        <div ClassName="app-container">
+            <AdminDashboard />
+            </div>
+    );
+};
+export default App;
+
